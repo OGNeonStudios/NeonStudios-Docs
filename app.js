6b31a9792de4bcb54b9e2680d3e3c30138098260
Arjav Jain <arjavjain29122007@gmail.com>
2026-04-16 22:48:00 +0530

    Fixed Search & NeonLottery

--- app.js
+++ app.js
@@ -147,6 +147,7 @@
     { title: 'NeonGlows', section: 'Products', href: 'docs/neonglows.html#introduction', keywords: 'neonglows glow effects cosmetics' },
     { title: 'NeonClans', section: 'Products', href: 'docs/neonclans.html#introduction', keywords: 'neonclans clans alliances treasury pvp premium' },
     { title: 'NeonTokenX', section: 'Products', href: 'docs/neontokenx.html#introduction', keywords: 'neontokenx tokensmp abilities tokens crafting economy premium' },
+    { title: 'NeonLottery', section: 'Products', href: 'docs/neonlottery.html#introduction', keywords: 'neonlottery lottery jackpot vaults economy premium gui tickets' },
   ];
 
   function initSearch() {
